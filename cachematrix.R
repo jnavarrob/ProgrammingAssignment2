fcdb979c25036a5363f90e0b70cff36260576eab
jnavarrob <javier.navarro.b@gmail.com>
2017-04-14 23:29:19 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 14Apr2017 - Javier Navarro B
+## Programming 2:  Given a matrix its Inverse is returned by these 2 functions using Lexical Scoping
 
-## Write a short comment describing this function
+## This function returns the Inverse of a matrix given as parameter in the variable m_JNB
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m_JNB = matrix()){
+      inv_JNB   <- NULL
+      setJNB    <- function(y_JNB){
+            m_JNB   <<- y_JNB
+            inv_JNB <<- NULL
+      }
+      getJNB    <- function() m_JNB
+      setInvJNB <- function(matinverse) inv_JNB <<- matinverse
+      getInvJNB <- function() inv_JNB
+      list(setJNB=setJNB,
+           getJNB=getJNB,
+           setInvJNB=setInvJNB,
+           getInvJNB=getInvJNB
+      )
 }
 
 
-## Write a short comment describing this function
+## This function returns the Inverse of a Matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m_JNB, ...) {
+      inv_JNB    <- m_JNB$getInvJNB()
+      if(!is.null(inv_JNB)) {
+            message("getting cached data")
+            return(inv_JNB)
+      }
+      matrix_JNB <- m_JNB$getJNB()
+      inv_JNB    <- solve(matrix_JNB)
+      m_JNB$setInvJNB(inv_JNB)
+      inv_JNB
 }
